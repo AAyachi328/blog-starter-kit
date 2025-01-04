2d15b78fd11e4b757f6f40ea568bc76e4b2416e9
AAyachi328 <110423661+AAyachi328@users.noreply.github.com>
2025-01-04 18:37:19 +0100

    Fix typo in intro component subtitle for improved clarity: changed "a ne pas manquer" to "à ne pas manquer".

--- src/app/_components/intro.tsx
+++ src/app/_components/intro.tsx
@@ -7,7 +7,7 @@ export function Intro() {
         Actus Tennis 🎾
       </h1>
       <h4 className="text-center md:text-left text-lg mt-5 md:pl-8">
-        Les actus du tennis a ne pas manquer !!!{" "}
+        Les actus du tennis à ne pas manquer !!!{" "}
        - {CMS_NAME}.
       </h4>
     </section>
