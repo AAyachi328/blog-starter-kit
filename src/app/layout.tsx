cad5db3b5a9b43862808c9d4ea10fc3f475df460
AAyachi328 <110423661+AAyachi328@users.noreply.github.com>
2025-01-05 10:09:46 +0100

    Update layout.tsx

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -65,7 +65,7 @@ export default function RootLayout({
         <SpeedInsights />
         <DailyNotifications />
         <div className="min-h-screen">{children}</div>
-        <Footer />
+        
       </body>
     </html>
   );
